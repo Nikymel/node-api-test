d68b3eb58a03ea6c7f6d494ccc69919f09cf3981
Nikita Melnikov <nikita.melnikov@and.digital>
2019-09-30 11:37:49 +0100

    Added a POST function

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = 8000;
 // Parse incoming messages to JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+// Enabling CORS
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -26,6 +27,7 @@ let testDB = [
     }
 ];
 
+// Get request for users
 app.get('/api/users', (req, res) => {
     res.status(200).send({
         success: 'true',
@@ -34,6 +36,7 @@ app.get('/api/users', (req, res) => {
     })
 });
 
+// Post request for users
 app.post('/api/users', (req, res) => {
     // If username is not received
     if (!req.body.username) {
@@ -59,7 +62,21 @@ app.post('/api/users', (req, res) => {
             data: null
         });
     }
-})
+
+    const user = {
+        username: req.body.username,
+        name: req.body.name,
+        age: req.body.age
+    };
+
+    testDB.push(user);
+
+    return res.status(201).send({
+        success: 'true',
+        message: 'user added succesfully',
+        data: user
+    });
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening on port ${PORT}`);
